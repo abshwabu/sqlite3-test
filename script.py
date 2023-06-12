8d23a6636eb10a7e5ffe4d67f2a587a86821bfba
ashwabu <abdulazizredi90@gmail.com>
2023-06-12 17:46:28 +0300

    separate function

--- script.py
+++ script.py
@@ -1,10 +1,17 @@
 import sqlite3
+def createTable():
+    db = sqlite3.connect('inventory.db')
 
-db = sqlite3.connect('inventory.db')
+    cur = db.cursor()
 
-cur = db.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS store(item TEXT,quantity INTIGER,price REAL)")
+    cur.execute("INSERT INTO store VALUES('book',10,15.50)")
+    db.commit()
+    db.close()
 
-cur.execute("CREATE TABLE IF NOT EXISTS store(item TEXT,quantity INTIGER,price REAL)")
-cur.execute("INSERT INTO store VALUES('book',10,15.50)")
-db.commit()
-db.close()
+def insert(item,qty,price):
+    db = sqlite3.connect('inventory.db')
+    cur = db.cursor()
+    cur.execute("INSERT INTO store VALUES(?,?,?)",(item,qty,price))
+    db.commit()
+    db.close()
